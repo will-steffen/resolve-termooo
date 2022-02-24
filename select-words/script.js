ca02670ba41248a8d27178df8b6e8ab79019280d
Adrianhermes <adrianhermes11@gmail.com>
2022-02-24 20:06:17 -0300

    Normalização de Palavras

--- select-words/script.js
+++ select-words/script.js
@@ -1,17 +1,48 @@
-const { readFile } = require('fs');
+const fs = require('fs');
 
 var inputFileName = 'palavras.txt';
 var outputFileName = '5-letters-palavras.txt';
 
 
-readFile(inputFileName, 'utf-8', (err, data) => {
+fs.readFile(inputFileName, 'utf-8', (err, data) => {
     if (err) throw err;
     var arr = data.split('\r\n');
     var outPutArray = [];
     for(let i = 0; i < arr.length; i++)
     {
         if(arr[i].length == 5){
-            outPutArray.push(arr[i]);
+            var palavra = normalizarPalavra(arr[i]);
+            outPutArray.push(palavra);
         }
     }
+    var outputdata  = outPutArray.join('\n');
+    fs.writeFile(outputFileName, outputdata, (err) => {
+        if (err) throw err;
+      console.log('O arquivo foi criado!');
+    });
 });
+
+var config = [
+    ['ú' , 'u'] ,
+    ['í' , 'i'],
+    ['ó' , 'o'],
+    ['á' , 'a'],
+    ['é' , 'e'],
+ ];
+
+function normalizarPalavra(palavra){
+    palavra = palavra.toLowerCase();
+    for(let i = 0 ; i < config.length ; i++ ){
+        palavra = palavra.replaceAll(config[i][0],config[i][1] );
+    }
+    return palavra;
+}
+
+
+String.prototype.replaceAll = function(a ,b){
+    var s = this;
+    while(s.indexOf(a) > -1 ){
+        s = s.replace (a , b);
+    }
+    return s;
+}
